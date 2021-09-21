9d9331fc9710dd9cc64e2a74b9d4b99223120445
Oplexitie <72892666+Oplexitie@users.noreply.github.com>
2021-09-21 02:41:28 +0200

    Add files via upload

---
+++ assets/data/Tiledata/gem.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="gem" tilewidth="15" tileheight="13" tilecount="5" columns="5">
+ <image source="../../images/goodies/gem.png" width="75" height="13"/>
+</tileset>
